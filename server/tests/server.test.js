ff016f09b40c848432d470ef5ca92ddfea6c9296
lap14tclc2 <honaz140@gmail.com>
2018-07-05 13:51:48 +0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -115,4 +115,45 @@ describe('GET /todos/:id', () => {
         .expect(404)
         .end(done)
     })
+});
+
+// testing DELETE method
+describe('DELETE /todos/:id', () => {
+    const hexId = todos[1]._id.toHexString();
+
+    it('should remove a todo', done => {
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect( res => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end( (err, res) =>{
+            if(err){
+                return done(err);
+            }
+            Todo.findById(hexId).then( todo => {
+                // export todo = null by using toNotExist() in library
+                expect(todo).toBeFalsy(); 
+                done();
+            }).catch(err => done(err));
+        })
+    });
+
+     it('should return 404 if todo not found', done => {
+        const hexId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done)
+     });
+
+     it('should return 404 if id invalid',done => {
+        request(app)
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done)
+     })
+
+
 })
